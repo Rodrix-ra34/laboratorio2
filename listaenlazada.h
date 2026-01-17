f8c56ebf6fd41df18540ba48a82f16b86d142969
EmelyFuentes <00126025@uca.edu.sv>
2026-01-16 20:04:02 -0600

    add: lista enlazada de estudiantes

---
+++ listaenlazada.h
@@ -0,0 +1,80 @@
+#ifndef LISTAENLAZADA_H
+#define LISTAENLAZADA_H
+
+#include <iostream>
+#include "estudiante.h"
+
+using namespace std;
+
+struct Nodo
+{
+    Estudiante* est;
+    Nodo* sig;
+
+    Nodo(Estudiante* e)
+    {
+        est = e;
+        sig = nullptr;
+    }
+};
+
+class ListaEnlazada
+{
+private:
+    Nodo* head;
+
+    float sumarNotas(Nodo* actual, int& contador)
+    {
+        if (actual == nullptr)
+            return 0;
+
+        contador++;
+        return actual->est->nota() + sumarNotas(actual->sig, contador);
+    }
+
+public:
+    ListaEnlazada()
+    {
+        head = nullptr;
+    }
+
+    void agregarEstudiante(string nombre, float nota)
+    {
+        Estudiante* nuevo = new Estudiante(nombre, nota);
+        Nodo* nuevoNodo = new Nodo(nuevo);
+
+        if (head == nullptr)
+        {
+            head = nuevoNodo;
+        }
+        else
+        {
+            Nodo* temp = head;
+            while (temp->sig != nullptr)
+                temp = temp->sig;
+
+            temp->sig = nuevoNodo;
+        }
+    }
+
+    void imprimirLista()
+    {
+        cout << "--- Lista de Estudiantes ---" << endl;
+        Nodo* temp = head;
+
+        while (temp != nullptr)
+        {
+            temp->est->imprimir();
+            temp = temp->sig;
+        }
+    }
+
+    float calcularPromedioRecursivo()
+    {
+        int total = 0;
+        float suma = sumarNotas(head, total);
+        return (total == 0) ? 0 : (suma / total);
+    }
+};
+
+#endif
