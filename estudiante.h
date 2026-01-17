9516535609c53b63b51b6e999df6c143c55fea18
Rodrix-ra34 <00167625@uca.edu.com>
2026-01-16 19:12:07 -0600

    add: clase de estudiante

---
+++ estudiante.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Estudiante
+{
+private:
+    string nombre;
+    float notafinal;
+
+public:
+    Estudiante(string n, float notaf) : nombre(n), notafinal(notaf) {}
+    float nota()
+    {
+        return notafinal;
+    }
+    void imprimir()
+    {
+        cout << nombre << " su nota es: " << notafinal << endl;
+    }
+};
